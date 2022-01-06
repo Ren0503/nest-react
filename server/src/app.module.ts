0e705195486da81dc25356fde94d5fb28d0eb127
Ren0503 <illuminati0440@gmail.com>
2022-01-06 15:23:41 +0700

    Add sever basic complete

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as ormOptions from './config/orm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserResolver } from './user/user.resolver';
-import { MessageResolver } from './message/message.resolver';
 import { RepoModule } from './repo/repo.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { context } from './db/loaders';
+import UserResolver from './user/user.resolver';
+import MessageResolver from './message/message.resolver';
 
+const gqlImports = [UserResolver, MessageResolver];
 @Module({
-  imports: [RepoModule],
+  imports: [
+    TypeOrmModule.forRoot(ormOptions),
+    RepoModule,
+    ...gqlImports,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+      installSubscriptionHandlers: true,
+      context,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, UserResolver, MessageResolver],
+  providers: [AppService],
 })
 export class AppModule {}
