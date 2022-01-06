55bb15f87b634196f2e489f21cfe8daf98c6d4bf
Ren0503 <illuminati0440@gmail.com>
2022-01-06 15:18:26 +0700

    Add resolvers

--- server/src/user/user.resolver.ts
+++ server/src/user/user.resolver.ts
@@ -1,4 +1,38 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import User from 'src/db/models/user.entity';
+import RepoService from 'src/repo/repo.service';
+import UserInput from './user.input';
 
-@Resolver()
-export class UserResolver {}
+@Resolver(() => User)
+export default class UserResolver {
+    constructor (private readonly repoService: RepoService) {}
+
+    @Query(() => [User])
+    public async getUsers(): Promise<User[]> {
+        return this.repoService.userRepo.find()
+    }
+
+    @Query(() => User, { nullable: true })
+    public async getUser(@Args('id') id: number): Promise<User> {
+        return this.repoService.userRepo.findOne(id);
+    }
+
+    @Mutation(() => User)
+    public async createOrLoginUser(
+        @Args('data') input: UserInput,
+    ): Promise<User> {
+        let user = await this.repoService.userRepo.findOne({
+            where: { email: input.email.toLowerCase().trim() },
+        });
+
+        if (!user) {
+            user = this.repoService.userRepo.create({
+                email: input.email.toLowerCase().trim(),
+            });
+
+            await this.repoService.userRepo.save(user);
+        }
+
+        return user;
+    }
+}
